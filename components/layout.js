6297be183674f8d531ca030ab907480460c4f0be
Tynan DeBold <tynan.debold@tradeshift.com>
2019-04-24 22:28:56 +0200

    first commit

---
+++ components/layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default ({ children, title = 'aagaard design studio' }) => (
+  <div>
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <style jsx global>{`
+      body {
+        background-color: #f7f7f7;
+        font-family: Helvetica Neue, Helvetica, sans-serif;
+        margin: auto;
+        max-width: 70ch;
+        padding: 2ch;
+      }
+    `}</style>
+    <header>
+      <h1>aagard design studio</h1>
+    </header>
+
+    {children}
+  </div>
+);
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layout';
+
+export default () => (
+  <Layout>
+    <div>The stuff will stuff itstuff here.</div>
+  </Layout>
+);
